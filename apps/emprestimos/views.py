ebb235cb70d089dd71608b5d241f3183258835fd
carlamagalhaes8 <carla.magalhaes404@gmail.com>
2025-12-18 21:12:04 -0300

    excluir e editar emprestimo

--- apps/emprestimos/views.py
+++ apps/emprestimos/views.py
@@ -1,5 +1,5 @@
 from django.contrib import messages
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, get_object_or_404
 
 from apps.emprestimos.forms import EmprestimoForm
 from apps.emprestimos.models import Emprestimo
@@ -21,4 +21,25 @@ def listar_emprestimos(request):
     template_name = 'emprestimos/listar_emprestimos.html'
     emprestimos = Emprestimo.objects.all()
     context = {'relacao_emprestimos': emprestimos}
+    return render(request, template_name, context)
+
+def editar_emprestimo(request, id):
+    template_name = 'emprestimos/form_emprestimo.html'
+    emprestimo = get_object_or_404(Emprestimo, id=id)
+    form = EmprestimoForm(request.POST or None, request.FILES or None, instance=emprestimo)
+    context = {'form': form}
+    if form.is_valid():
+        form.save()
+        messages.success(request, 'Os dados foram atualizados com sucesso.')
+        return redirect('emprestimos:listar_emprestimos')
+    return render(request, template_name, context)
+
+def excluir_emprestimo(request, id):
+    template_name = 'emprestimos/excluir_emprestimo.html'
+    emprestimo = Emprestimo.objects.get(id=id)
+    context = {'emprestimo': emprestimo}
+    if request.method == "POST":
+        emprestimo.delete()
+        messages.error(request, 'O emprestimo foi excluído com sucesso.')
+        return redirect('emprestimos:listar_emprestimos')
     return render(request, template_name, context)
